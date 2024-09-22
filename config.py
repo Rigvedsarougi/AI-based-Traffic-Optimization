19e2cde9dbea07c9ed055de2a6b400b1a971df14
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2024-09-22 15:15:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TRAFFIC_LIGHT_TIMINGS = {
+    "short": 30,
+    "medium": 45,
+    "long": 60
+}
+
+RL_SETTINGS = {
+    "learning_rate": 0.1,
+    "discount_factor": 0.99,
+    "exploration_rate": 0.1
+}
